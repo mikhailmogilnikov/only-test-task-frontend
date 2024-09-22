07f9c62e7757750c16aeb4751f4bcb4b1c8b2603
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-09-22 23:32:38 +0300

    fix: mobile styles in gallery

--- src/pages/home/ui/index.tsx
+++ src/pages/home/ui/index.tsx
@@ -1,4 +1,4 @@
-import { MockTimelines } from '../config/mock-timeline';
+import { MockTimelines1, MockTimelines2 } from '../config/mock-timeline';
 
 import styles from './styles.module.scss';
 
@@ -7,8 +7,8 @@ import { TimelinesBlock } from '@/widgets/timelines-block';
 export const HomePage = () => {
   return (
     <main className={styles.wrapper}>
-      <TimelinesBlock timelines={MockTimelines} />
-      <TimelinesBlock timelines={MockTimelines} />
+      <TimelinesBlock timelines={MockTimelines1} />
+      <TimelinesBlock timelines={MockTimelines2} />
     </main>
   );
 };
